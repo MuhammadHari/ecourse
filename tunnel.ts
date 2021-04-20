7473bd900ddee85fa31a616635c2c1b7a3cd75af
imandidik12 <imandidikr@gmail.com>
2021-04-20 23:52:31 +0700

    fix tunnel command

--- tunnel.ts
+++ tunnel.ts
@@ -1,8 +1,11 @@
 import localTunnel from "localtunnel";
-import child from "child_process";
+import * as process from "child_process";
+import opener from "opener";
 
-child.exec("php artisan serve --host=0.0.0.");
+process.exec("php artisan serve --host=0.0.0.");
+
 (async () => {
- const tunnel = await localTunnel({ port: "8000", subdomain: "e-course" });
-  console.log(tunnel.url);
+  const tunnel = await localTunnel({ port: "8000", subdomain: "e-course" });
+  opener(tunnel.url);
+  console.log(tunnel.url)
 })();
